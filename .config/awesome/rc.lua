b3f8ef95628124b92a8ccbc96c66348a4b2cfb39
Halim Zaghdoudi <halim.zag@gmx.de>
2026-02-02 19:48:35 +0100

    Update rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -160,5 +160,5 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- AUTOSTART
-awful.spawn.with_shell("xrandr --output DP-0 --mode 1920x1080 --rate 239.96")
-awful.spawn.with_shell("numlockx on")
+--awful.spawn.with_shell("xrandr --output DP-0 --mode 1920x1080 --rate 239.96")
+--awful.spawn.with_shell("numlockx on")
