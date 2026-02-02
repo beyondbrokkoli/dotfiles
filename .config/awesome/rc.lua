53cdb91c19d2fb10474e2440d7ff2008d8e967d1
Halim Zaghdoudi <halim.zag@gmx.de>
2026-02-02 19:47:44 +0100

    Update rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -159,5 +159,6 @@ end)
 --client.connect_signal("mouse::enter", function(c) c:emit_signal("request::activate", "mouse_enter", {raise = false}) end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+-- AUTOSTART
 awful.spawn.with_shell("xrandr --output DP-0 --mode 1920x1080 --rate 239.96")
 awful.spawn.with_shell("numlockx on")
