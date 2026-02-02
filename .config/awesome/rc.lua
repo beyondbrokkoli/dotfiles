2f8ef44a19f82ea41631c861e68eb564f2e10223
Halim Zaghdoudi <halim.zag@gmx.de>
2026-02-02 19:47:00 +0100

    Update rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -140,7 +140,7 @@ root.keys(globalkeys)
 awful.rules.rules = {
     { rule = { }, properties = { border_width = beautiful.border_width, border_color = beautiful.border_normal, focus = awful.client.focus.filter, raise = true, keys = clientkeys, buttons = clientbuttons, screen = awful.screen.preferred, placement = awful.placement.no_overlap+awful.placement.no_offscreen } },
     { rule_any = { instance = { "DTA", "copyq", "pinentry" }, class = { "Arandr", "Blueman-manager", "Gpick", "Kruler", "Sxiv", "Wpa_gui", "veromix" } }, properties = { floating = true } },
-    { rule_any = { type = { "normal", "dialog" } }, properties = { titlebars_enabled = true } }
+    { rule_any = { type = { "normal", "dialog" } }, properties = { titlebars_enabled = false } }
 }
 
 client.connect_signal("manage", function (c) if awesome.startup and not c.size_hints.user_position and not c.size_hints.program_position then awful.placement.no_offscreen(c) end end)
